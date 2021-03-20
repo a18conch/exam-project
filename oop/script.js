055808a1130c73c038decb7f54f4842310c38e1d
Constantin Christoforidis <c_christoforidis@hotmail.com>
2021-03-21 00:11:13 +0100

    fixed syntax error and refactored obj parser

--- oop/script.js
+++ oop/script.js
@@ -4,6 +4,7 @@ var displayHeight;
 
 import { mat4, vec3, vec4, quat, glMatrix } from '/gl-matrix/index.js'
 import { VisualObject } from '/visual-object.js'
+import { parseObj } from '/parse-obj.js'
 
 async function main(vertexShaderSource, fragmentShaderSource) {
 
@@ -75,43 +76,6 @@ async function main(vertexShaderSource, fragmentShaderSource) {
     });
   }
 }
-
-function parseObj(text) {
-  const geometricVertices = getValuesFromPattern(text, /v\s.*/gm);
-  const textureVertices = getValuesFromPattern(text, /vt\s.*/gm);
-  const vertexNormals = getValuesFromPattern(text, /vn\s.*/gm);
-  const faces = getValuesFromPattern(text, /f\s.*/gm, true);
-
-
-  return { geometricVertices, textureVertices, vertexNormals, faces };
-}
-
-function getValuesFromPattern(text, pattern, faces) {
-  let array = [];
-  const matches = text.match(pattern);
-  for (const geoVert of matches) {
-    if (faces)
-      array.push(getFloatsFromStringFaces(geoVert));
-    else
-      array.push(getFloatsFromString(geoVert));
-  }
-  return array;
-}
-
-function getFloatsFromStringFaces(text) {
-  const numbers = text.match(/-?[0-9]\d*(\.\d+)?/g);
-  return {
-    x: { v: numbers[0], vt: numbers[1], vn: numbers[2] },
-    y: { v: numbers[3], vt: numbers[4], vn: numbers[5] },
-    z: { v: numbers[6], vt: numbers[7], vn: numbers[8] }
-  };
-}
-
-function getFloatsFromString(text) {
-  const [x, y, z] = text.match(/-?[0-9]\d*(\.\d+)?/g);
-  return { x: parseFloat(x), y: parseFloat(y), z: parseFloat(z) };
-}
-
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
